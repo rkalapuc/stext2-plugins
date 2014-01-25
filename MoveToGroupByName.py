263deedfdd045d5a03ff13c460fbb65f76da61d4
rkalapuc <rkalapuc@gmail.com>
2014-01-25 13:19:41 +0200

    Added correct version of a file

--- MoveToGroupByName.py
+++ MoveToGroupByName.py
@@ -1,26 +1,22 @@
 import sublime, sublime_plugin
 class MoveToGroupByName(sublime_plugin.WindowCommand):
- def run(self, direction, test):
-   pkg_name = "move_to_group_by_name"
-   window = sublime.active_window()
-   view = window.active_view()
-   ret = window.get_view_index(view)
-   print 'test=', test
-   if ret == -1:
-     return
-   (group, index) = ret
-   if group < 0 or index < 0:
-     return
-   views = window.views_in_group(window.active_group())
-   num_views = len(views)
-   oldIndex = index
-   if direction == "left":
-     if index > 0:
-       index -= 1
-   elif direction == "right":
-     if index < num_views - 1:
-       index += 1
-   else:
-     print 'Unrecognized direction1:',direction+'. Use left or right.'
-   if oldIndex != index:
-     window.set_view_index(view, group, index)
+  def run(self, vname, gindex, vindex):
+    window = sublime.active_window()
+    views = window.views()
+
+    targetView = None
+    for view in views:
+      if view.name() == vname:
+        targetView = view
+
+    if targetView is  None:
+      print 'View [' + vname+ '] not found!'
+      return
+
+    gcount = window.num_groups()
+
+    if gindex < 0 or gindex >= gcount:
+      print 'Invalid group index [',gindex,'] specified!'
+      return
+
+    window.set_view_index(targetView, gindex, vindex)
